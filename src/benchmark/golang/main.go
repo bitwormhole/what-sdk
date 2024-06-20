2487a069d3ec15f3c2b27a1e0885a76a471edae7
xukun <xukun17@sina.com>
2024-06-20 18:11:24 +0800

    调整了 benchmark 的命令行启动参数

--- src/benchmark/golang/main.go
+++ src/benchmark/golang/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"os"
 
 	"github.com/bitwormhole/what-sdk/modules/whatsdk"
@@ -9,10 +10,25 @@ import (
 
 func main() {
 
+	var configFilePath = ""
+	flag.StringVar(&configFilePath, "p", "", "本地配置文件的路径")
+	flag.Parse()
+
+	if configFilePath == "" {
+		panic("缺少命令行参数：-p")
+	}
+
+	addProps := map[string]string{
+		"application.properties.file":    configFilePath,
+		"application.properties.enabled": "1",
+	}
+
+	// run
 	m := whatsdk.ModuleBenchmark()
 	args := os.Args
 
 	i := starter.Init(args)
+	i.GetProperties().Import(addProps)
 	i.MainModule(m)
 	i.WithPanic(true)
 	i.Run()
